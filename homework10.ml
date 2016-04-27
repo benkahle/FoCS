853fa51e7ecbaabea69cec576b8fcc9b694a0d7a
Ben Kahle <ben@kahle.org>
2016-04-26 22:07:49 -0400

    finish hw10

--- homework10.ml
+++ homework10.ml
@@ -1,10 +1,10 @@
-(* 
+(*
 
 HOMEWORK 10
 
-Name: 
+Name: Ben Kahle
 
-Email:
+Email: bernard.kahle@students.olin.edu
 
 Remarks, if any:
 
@@ -19,7 +19,7 @@ Remarks, if any:
  * own implementation.
  *
  * Always make sure you can #use this file before submitting it.
- * Do that in a _fresh_ OCaml shell 
+ * Do that in a _fresh_ OCaml shell
  * It has to load without any errors.
  *
  *)
@@ -40,7 +40,7 @@ let sample = Node(10,Node(3,Node(7,Empty,Empty),
 (* Printing an integer binary tree *)
 
 let pbt bt =
-  let rec loop bt depth = 
+  let rec loop bt depth =
     match bt with
     | Empty -> ()
     | Node(n,left,right) ->
@@ -55,40 +55,63 @@ let pbt bt =
 
 (* Q1 *)
 
-let rec size t = failwith ("size not implemented")
+let rec size t =
+  match t with
+    | Empty -> 0
+    | Node (v, l, r) -> 1 + size l + size r
+
+let rec sum t =
+  match t with
+    | Empty -> 0
+    | Node (v, l, r) -> v + sum l + sum r
 
 
-let rec sum t = failwith ("sum not implemented")
+let rec height t =
+  match t with
+    | Empty -> 0
+    | Node (v, l, r) -> 1 + max (height l) (height r)
 
 
-let rec height t = failwith ("height not implemented")
+let rec fringe t =
+  match t with
+    | Empty -> []
+    | Node (v, l, r) -> if (l == Empty && r == Empty) then [v] else (fringe l) @ (fringe r)
 
 
-let rec fringe t = failwith ("fringe not implemented")
+let rec map f t =
+  match t with
+    | Empty -> Empty
+    | Node (v, l, r) -> Node (f v, map f l, map f r)
 
 
-let rec map f t = failwith ("map not implemented")
+let rec fold f t b =
+  match t with
+    | Empty -> b
+    | Node (v, l, r) -> f v (fold f l b) (fold f r b)
+
+let preorder t = fold (fun v l r -> [v] @ l @ r) t []
 
 
-let rec fold f t b = failwith ("fold not implemented")
+let postorder t = fold (fun v l r -> l @ r @ [v]) t []
 
 
-let preorder t = failwith ("preorder not implemented")
+let inorder t = fold (fun v l r -> l @ [v] @ r) t []
 
 
-let postorder t = failwith ("postorder not implemented")
+let rec bst_insert t x =
+  match t with
+    | Empty -> Node (x, Empty, Empty)
+    | Node (v, l, r) -> if (v > x) then Node (v, bst_insert l x, r) else Node (v, l, bst_insert r x)
 
 
-let inorder t = failwith ("inorder not implemented")
+let rec bst_lookup t x =
+  match t with
+    | Empty -> false
+    | Node (v, l, r) -> if (v = x) then true else (if (v > x) then bst_lookup l x else bst_lookup r x)
 
 
-let rec bst_insert t x = failwith ("bst_insert not implemented")
-
-
-let rec bst_lookup t x = failwith ("bst_lookup not implemented")
-
-
-let rec bstify t =  failwith ("bstify not implemented")
+let rec bstify t =
+  List.fold_left (fun t v -> bst_insert t v) Empty (preorder t)
 
 
 let avl_insert t x = failwith ("avl_insert not implemented")
